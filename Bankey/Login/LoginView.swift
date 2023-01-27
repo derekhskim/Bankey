63d584bff22dfd83640e1c5abb08e4e92c1330ae
[treasure3210] <treasure3210@gmail.com>
2023-01-27 13:55:28 -0700

    In process of adding textfield for the loginview

--- Bankey/Login/LoginView.swift
+++ Bankey/Login/LoginView.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class LoginView: UIView {
     
+    let usernameTextField = UITextField()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
@@ -31,9 +33,34 @@ extension LoginView {
     func style() {
         translatesAutoresizingMaskIntoConstraints = false
         backgroundColor = .orange
+        
+        usernameTextField.translatesAutoresizingMaskIntoConstraints = false
+        usernameTextField.placeholder = "Username"
+        usernameTextField.delegate = self
     }
     
     func layout() {
         
+    }
+}
+
+// MARK: - UITextFieldDelegate
+extension LoginView: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        usernameTextField.endEditing(true)
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        <#code#>
     }
 }
