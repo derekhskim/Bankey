d760532bba9d6dbeb1897b4300bca763967a1d77
[treasure3210] <treasure3210@gmail.com>
2023-03-13 21:03:05 -0600

    synchronize no longer needed as of iOS 12+

--- Bankey/Utils/LocalState.swift
+++ Bankey/Utils/LocalState.swift
@@ -19,7 +19,6 @@ public class LocalState {
         }
         set(newValue) {
             UserDefaults.standard.set(newValue, forKey: Keys.hasOnboarded.rawValue)
-            UserDefaults.standard.synchronize()
         }
     }
 }
